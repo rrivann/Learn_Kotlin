9f52a9df07c940da214d8fc3d5bb6e8eced944b2
Rivan Albani Ray <rivanalbaniray88@gmail.com>
2023-01-19 13:47:43 +0700

    navigation done

---
+++ MyTabLayout/app/src/main/java/com/dicoding/mytablayout/SectionsPagerAdapter.kt
@@ -0,0 +1,23 @@
+package com.dicoding.mytablayout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class SectionsPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
+    var appName: String = ""
+
+    override fun createFragment(position: Int): Fragment {
+        var fragment = HomeFragment()
+        fragment.arguments = Bundle().apply {
+            putInt(HomeFragment.ARG_SECTION_NUMBER, position + 1)
+            putString(HomeFragment.ARG_NAME,appName)
+        }
+        return fragment
+    }
+
+    override fun getItemCount(): Int {
+        return 3
+    }
+}
